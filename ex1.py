b5934b2b539b2093e7d9e0fe4993427fea44e281
SMWalsh <smwalsh0@gmail.com>
2014-07-29 19:10:16 -0500

    ex1-3

---
+++ ex1.py
@@ -0,0 +1,16 @@
+# print "Hello World!" #is a comment
+print "Hello Again:"
+print ""
+print ''
+print "I like typing this."
+print "This is fun."
+print 'Yay! Printing.'
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
+
+
+"""
+Print "" prints a black line on the screen.
+So does Print ''
+The multiline comments can be used to commentout multiple statements
+"""
